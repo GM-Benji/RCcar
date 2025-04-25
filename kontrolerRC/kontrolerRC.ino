29fdd3025bfad7c6e979eb2609038661715ef963
benji <beniamin.raczynski@gmail.com>
2025-04-25 21:38:34 +0200

    nie ten kod xd

--- kontrolerRC/kontrolerRC.ino
+++ kontrolerRC/kontrolerRC.ino
@@ -1,28 +1,51 @@
-#include <VirtualWire.h>
+#include <RCSwitch.h>
 const byte pin1=PC1;
 const byte pin2=PC2;
 const byte pin3=PD2;
 const byte pin4=PD4;
-int16_t Rx;
-int16_t Ry;
+uint16_t Rx;
+uint16_t Ry;
 int16_t calibRx = 0;
 int16_t calibRy = 0;
-int16_t switch_bounceOC = 0;
-int16_t switch_bounce = 0;
+uint16_t switch_bounceOC = 0;
+uint16_t switch_bounce = 0;
+uint8_t sndX;
+uint8_t sndY;
 uint8_t xsign;
-uint8_t xyign;
-
-ISR(TIMER1_COMPB_vect)
+uint8_t ysign;
+uint16_t sdata;
+RCSwitch mySwitch = RCSwitch();
+void setup()
 {
-  OCR1B += 25000; // Advance The COMPA Register
-  Rx = analogRead(pin1) - calibRx;
-  Ry = analogRead(pin2) - calibRy;
-  Serial.print(Rx);
-  Serial.print("  ");
-  Serial.println(Ry);
-  uint8_t data[3] = {69,21,37};
-  uint8_t *senddata = data;
-  vw_send(senddata,3);
+  mySwitch.enableTransmit(pin4);
+  Serial.begin(9600);
+  pinMode(pin1,INPUT);
+  pinMode(pin2,INPUT);
+  //TCCR1A = 0;           // Init Timer1
+  TCCR2A = 0;           // Init Timer1
+  TCCR2A |= B00000100;  // Prescalar = 64
+  //OCR1A = 25000;        // Timer CompareA Register
+  OCR2A = 25000;        // Timer CompareB Register
+  TIMSK2 |= B00000010;  // Enable Timer COMPA Interrupt
+  pinMode(pin3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pin3), SW, FALLING);
+  //calibRx = analogRead(pin1);
+  //calibRy = analogRead(pin2);
+  
+}
+ISR(TIMER2_COMPA_vect)
+{
+  OCR2A += 25000; // Advance The COMPA Register
+  Rx = analogRead(pin1);
+  Ry = analogRead(pin2);
+  // Serial.print(Rx);
+  // Serial.print("  ");
+   //Serial.println(Ry);
+  if(Rx<65)Rx=65;
+  mySwitch.send(Rx, 11);
+  sdata = Ry | 10000000000;
+  Serial.println(Rx);
+  mySwitch.send(sdata, 11);
   if(switch_bounce == 65635)
   {
     switch_bounce = 0;
@@ -30,30 +53,9 @@ ISR(TIMER1_COMPB_vect)
   }
   else switch_bounce++;
 }
-
-void setup()
-{
-  vw_setup(4000);// speed of data transfer in bps, can max out at 10000
-  Serial.begin(9600);
-  Serial.println("dupa");
-  pinMode(pin1,INPUT);
-  pinMode(pin2,INPUT);
-  TCCR1B = 0;           // Init Timer1
-  TCCR1B |= B00000011;  // Prescalar = 64
-  OCR1B = 25000;        // Timer CompareB Register
-  TIMSK1 |= B00000100;  // Enable Timer COMPA Interrupt
-  pinMode(pin3, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(pin3), SW, FALLING);
-  calibRx = analogRead(pin1);
-  calibRy = analogRead(pin2);
-  vw_set_tx_pin(pin4); 
-  
-  
-  
-}
-
 void loop() {
   // put your main code here, to run repeatedly:
+  
 }
 void SW()
 {
